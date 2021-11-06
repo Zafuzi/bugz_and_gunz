a29a506d85159a5dea38ad0bf443a48d6b8e0f63
Zafuzi <zacharyfoutz@gmail.com>
2021-11-06 09:11:34 -0700

    * added starter canvas and idea settings

--- client/main.js
+++ client/main.js
@@ -3,3 +3,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+const GAME_WIDTH = 1200;
+const GAME_HEIGHT = 900;
+
+Template.gameCanvas.onCreated(function()
+{
+	this.paused = new ReactiveVar(true);
+	this.width = new ReactiveVar(GAME_WIDTH);
+	this.height = new ReactiveVar(GAME_HEIGHT);
+});
+
